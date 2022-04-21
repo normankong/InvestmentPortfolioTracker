049faa9b4162c25b4a4531f683f0cbc34686ca68
Norman Kong <normankong@gmail.com>
2022-04-21 13:05:36 -0700

    Add CORS support

--- sam-sentiment-website/client/src/api/apiConfig.js
+++ sam-sentiment-website/client/src/api/apiConfig.js
@@ -1,7 +1,8 @@
+
 const config = {
   stocks: {
     search: `https://cs5610-ivt-00001.herokuapp.com/search/`,
-    quote : `https://c525wqdrve.execute-api.us-east-1.amazonaws.com/Prod/`
+    quote : process.env.REACT_APP_REQUEST_URL
     // quote : `/mock/quote.json?symbol=`
   },
 };
